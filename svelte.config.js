9be2a39a39263a64026fcaa691749dd755565571
sbutler-gh <sam@ysli.be>
2021-07-04 21:35:56 +0200

    Hosting deployment config

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,14 @@
+import preprocess from "svelte-preprocess";
 import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter(),
 		target: '#svelte',
 		vite: {
 			optimizeDeps: {
-			  adapter: adapter(),
 			  include: ["fastq"]			
 			}
 		  }
